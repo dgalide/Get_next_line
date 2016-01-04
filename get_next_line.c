43e02c60ea2542d2ffc0b07c91a168d0feb9c7fc
damien galidie <Dams@MacBook-Pro-de-damien.local>
2016-01-05 00:01:18 +0100

    5/01/16

---
+++ get_next_line.c
@@ -0,0 +1,105 @@
+#include "get_next_line.h"
+
+int		ft_check_line(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n' || str[i] == '\0')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int		ft_get_all(int const fd, char **rest)
+{
+	int	ret;
+	char	buff[BUFF_SIZE];
+	char	*tmp;
+
+	while((ret = read(fd, buff, BUFF_SIZE)))	
+	{
+		if (*rest)
+		{
+			tmp = *rest;
+			*rest = ft_strjoin(*rest, ft_strsub(buff, 0, ret));
+			free(tmp);
+		}
+		else
+			*rest = ft_strsub(buff, 0, ret);
+	}
+	return (ret);
+}
+
+char		*ft_get_line(char **str)
+{
+	int	i;
+	char	*tmp;
+	char	*cpy;
+
+	if (!*str)
+		return (NULL);	
+	i = ft_check_line(*str);
+	if (i == 0)
+	{
+		cpy = (char *)malloc(sizeof(char));
+		cpy[0] = '\0';
+	}
+	else
+		cpy = ft_strsub(*str, 0, i);
+	if ((*str)[i + 1])
+	{
+		tmp = *str;
+		*str = ft_strsub(*str, (i + 1), ft_strlen(*str) - (i + 1));
+		free(tmp);
+	}
+	else
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return (cpy);
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	static char	*rest;
+	int	i;
+	int	j;
+
+	if (rest)
+		*line = ft_get_line(&rest);
+	else
+	{
+		i = ft_get_all(fd, &rest);
+		*line = ft_get_line(&rest);
+		if (i == -1)
+			return (i);
+	}
+	if (rest == NULL)
+		return (0);
+	else
+		return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	int fd;
+	int	i;
+	int	j;
+	char *line;
+
+	fd = open(argv[1], O_RDONLY);
+	i = 1;
+	j = 0;
+	//while (i != 0)
+	//{
+		i = get_next_line(fd, &line);
+		printf("%s\n", line);
+	//}
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,14 @@
 #ifndef GET_NEXT_LINE_H
  #define GET_NEXT_LINE_H
- #define BUFF_SIZE 1
+ #define BUFF_SIZE 10000
 
  #include <stdio.h>
  #include <fcntl.h>
  #include "libft.h"
 
 int		get_next_line(int const fd, char **line);
-int		ft_save(char *buff, char *save, char *rest);
+int		ft_get_all(int const fd, char **rest);
+int		ft_check_line(char *str);
+char		*ft_get_line(char **str);
 
 #endif
