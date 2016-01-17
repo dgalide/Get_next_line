095199a466d89f7c79ad68d3d17f7abac9b96664
Damien GALIDE <dgalide@e3r2p20.42.fr>
2016-01-17 19:58:19 +0100

    17/01/16

--- get_next_line.h
+++ get_next_line.h
@@ -4,11 +4,16 @@
 
  #include <stdio.h>
  #include <fcntl.h>
- #include "libft/libft.h"
+ #include "libft/includes/libft.h"
 
-int		get_next_line(int const fd, char **line);
-int		ft_get_all(int const fd, char **rest);
-int		ft_check_line(char *str);
-char		*ft_get_line(char **str);
+typedef struct				s_rest
+{
+	char					*rest;
+	int						fd;
+}							t_rest;
+
+int							get_next_line(int const fd, char **line);
+int							ft_check_line(char *str);
+char						*ft_get_line(char **str, int ret);
 
 #endif
